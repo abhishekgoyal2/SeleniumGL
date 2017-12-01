5b66dfccd6013e652edc60d8ded18032efb7d53c
abhishek.goyal2 <abhi25goel@gmail.com>
2017-12-01 15:20:54 +0530

    Commit changes

---
+++ src/test/java/com/GL/CQ/pagelibrary/Payment.java
@@ -0,0 +1,17 @@
+package com.GL.CQ.pagelibrary;
+
+public class Payment {
+
+	public static void main(String[] args)
+	
+	{
+	Signin signin =new Signin();
+	
+	signin.enterCreateEmail("abc@xyz.com");
+	signin.clickonSignin();
+	signin.clickonsigninbutton();
+	
+	
+	}
+	
+}
--- src/test/java/com/GL/CQ/pagelibrary/Signin.java
+++ src/test/java/com/GL/CQ/pagelibrary/Signin.java
@@ -7,22 +7,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class Signin {
-
-WebDriver driver;
+public class Signin 
+{
+     WebDriver driver;
 	 static Logger log = Logger.getLogger(Signin.class.getName());
-	WebElement Signinlink= driver.findElement(By.xpath("/html/body/div/div[1]/header/div[2]/div/div/nav/div[1]/a"));
+	 WebElement Signinlink= driver.findElement(By.xpath("/html/body/div/div[1]/header/div[2]/div/div/nav/div[1]/a"));
 	 WebElement CreateEmail= driver.findElement(By.xpath("//*[@id='email_create']"));
 	 WebElement registeredemail= driver.findElement(By.xpath("//*[@id=email]"));
 	 WebElement registeredpassword= driver.findElement(By.xpath("//*[@id='passwd']"));
 	 WebElement Siginbutton= driver.findElement(By.xpath("//*[@id='SubmitLogin']"));
-	 
+	
+	 /**
+	   * this method click on signinlink of loging page 
+	  */
 	 public void clickonSignin()
 	 {
 		log.info("clicking on signin link ");
 		 Signinlink.click();
 		
 	 }
+	 
+	 /* this method click will enter create email address link */
 
 	 public void enterCreateEmail(String emailaddress)
 	 {
@@ -31,6 +36,8 @@ WebDriver driver;
 		 CreateEmail.sendKeys(emailaddress);
 	 }
 	 
+	 /* this method enter registered email address link */
+
 	 public void enterregisteremail(String regemailaddress)
 	 {
 			log.info("entering registered email address ");
@@ -38,12 +45,20 @@ WebDriver driver;
 		 registeredemail.sendKeys(regemailaddress);
 	 }
 	 
+	 /* this method enter registered password address link */
+
+	 
 	 public void enterregisterpassword(String regpassword)
 	 {
 			log.info("entering registered Password ");
 
 		 registeredpassword.sendKeys(regpassword);
 	 }
+	 
+	 /** 
+	   * this method clicks on Signin button 
+	  */
+
 	 
 	 public void clickonsigninbutton()
 	 {
